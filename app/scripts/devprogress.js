66db50486b57c878017b6526c6e8a22f3f6bef3b
Ahliddin Gaffar <ahliddin.gaffar@gmail.com>
2014-06-03 02:39:43 +0200

    Final pictures for devprogress

--- app/scripts/devprogress.js
+++ app/scripts/devprogress.js
@@ -3,29 +3,23 @@ function processProgressInput (val) {
 	var img = $('#progressimg');
 	var pVal = $('#pValue');
 
-	if (val < 10) {
+	if (val < 20) {
 		img.attr('src','images/prog1.jpg');
 	}
-	else if (val < 20) {
-		img.attr('src','images/prog2.jpg');
-	}
-	else if (val < 30) {
-		img.attr('src','images/prog3.jpg');
-	}
-	else if (val < 50) {
+	else if (val < 40) {
 		img.attr('src','images/prog2.jpg');
 	}
 	else if (val < 60) {
 		img.attr('src','images/prog3.jpg');
 	}
 	else if (val < 80) {
-		img.attr('src','images/prog2.jpg');
+		img.attr('src','images/prog4.jpg');
 	}
 	else if (val < 100) {
-		img.attr('src','images/prog3.jpg');
-	}
+		img.attr('src','images/prog5.jpg');
+	}	
 	else {
-		img.attr('src','images/prog3.jpg');
+		img.attr('src','images/prog6.jpg');
 	}
 	pVal.text (val + ' %');
 
